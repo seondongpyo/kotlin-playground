46e298c8dd0e97e552e2b2fd88b4ffe4d2e6bef7
seondongpyo <seondongpyo@gmail.com>
2022-04-19 22:43:59 +0900

    test: 자동차 이름이 6글자 이상일 경우 예외 발생

--- src/test/kotlin/racingcar/domain/NameTest.kt
+++ src/test/kotlin/racingcar/domain/NameTest.kt
@@ -1,8 +1,10 @@
 package racingcar.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
 
 class NameTest {
 
@@ -10,5 +12,12 @@ class NameTest {
     @Test
     fun valid() {
         assertThat(Name("james")).isEqualTo(Name("james"))
+    }
+
+    @DisplayName("유효하지 않은 자동차 이름 - 6글자 이상")
+    @Test
+    fun invalid() {
+        assertThatThrownBy { Name("jackson") }
+            .isInstanceOf(IllegalArgumentException::class.java)
     }
 }
