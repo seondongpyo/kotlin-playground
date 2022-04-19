889badd18eb9fe721e16dc62e82e186bb3d084cf
seondongpyo <seondongpyo@gmail.com>
2022-04-19 22:59:45 +0900

    test: 최초 자동차 위치 값 테스트

--- src/main/kotlin/racingcar/domain/Position.kt
+++ src/main/kotlin/racingcar/domain/Position.kt
@@ -4,6 +4,16 @@ class Position(
     private var value: Int
 ) {
 
+    constructor(): this(DEFAULT_VALUE)
+
+    companion object {
+        private const val DEFAULT_VALUE = 0
+    }
+
+    fun value(): Int {
+        return value
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
--- src/test/kotlin/racingcar/domain/PositionTest.kt
+++ src/test/kotlin/racingcar/domain/PositionTest.kt
@@ -11,4 +11,10 @@ class PositionTest {
     fun valid() {
         assertThat(Position(1)).isEqualTo(Position(1))
     }
+
+    @DisplayName("최초 자동차 위치는 0")
+    @Test
+    fun initial() {
+        assertThat(Position().value()).isZero
+    }
 }
