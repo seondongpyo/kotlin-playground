deb678bbdfaf8cb0e0aac288ec5ff5f91d4fa93d
seondongpyo <seondongpyo@gmail.com>
2022-04-19 22:51:05 +0900

    refactor: 유효하지 않은 자동차 이름 검증 시, 공백 인수 전달

--- src/test/kotlin/racingcar/domain/NameTest.kt
+++ src/test/kotlin/racingcar/domain/NameTest.kt
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class NameTest {
 
@@ -14,10 +15,11 @@ class NameTest {
         assertThat(Name("james")).isEqualTo(Name("james"))
     }
 
-    @DisplayName("유효하지 않은 자동차 이름 - 6글자 이상")
-    @Test
-    fun invalid() {
-        assertThatThrownBy { Name("jackson") }
+    @DisplayName("유효하지 않은 자동차 이름 - 공백 또는 6글자 이상")
+    @ValueSource(strings = ["", "jackson"])
+    @ParameterizedTest
+    fun invalid(value: String) {
+        assertThatThrownBy { Name(value) }
             .isInstanceOf(IllegalArgumentException::class.java)
     }
 }
