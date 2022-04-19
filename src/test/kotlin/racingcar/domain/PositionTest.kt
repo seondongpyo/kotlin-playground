7f759c6ffbf10a5fb9b1f5a01135eb631b3e8a62
seondongpyo <seondongpyo@gmail.com>
2022-04-19 23:00:48 +0900

    test: 유효하지 않은 자동차 위치 값

--- src/test/kotlin/racingcar/domain/PositionTest.kt
+++ src/test/kotlin/racingcar/domain/PositionTest.kt
@@ -1,6 +1,7 @@
 package racingcar.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
@@ -16,5 +17,12 @@ class PositionTest {
     @Test
     fun initial() {
         assertThat(Position().value()).isZero
+    }
+
+    @DisplayName("유효하지 않은 자동차 위치 - 음수")
+    @Test
+    fun invalid() {
+        assertThatThrownBy { Position(-1) }
+            .isInstanceOf(IllegalArgumentException::class.java)
     }
 }
