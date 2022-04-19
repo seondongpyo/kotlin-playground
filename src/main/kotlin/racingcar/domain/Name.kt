e8b998af55509574751219056222071f6e69e914
seondongpyo <seondongpyo@gmail.com>
2022-04-19 22:46:05 +0900

    feat: 자동차 이름 생성 시 길이 검증

--- src/main/kotlin/racingcar/domain/Name.kt
+++ src/main/kotlin/racingcar/domain/Name.kt
@@ -4,6 +4,16 @@ class Name(
     private val value: String
 ) {
 
+    init {
+        if (value.isEmpty() || value.length > MAX_LENGTH) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    companion object {
+        private const val MAX_LENGTH = 5
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
