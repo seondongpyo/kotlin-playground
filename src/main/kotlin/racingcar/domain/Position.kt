0d1884753914117f930589ee92322c7d7d83ddbb
seondongpyo <seondongpyo@gmail.com>
2022-04-19 22:56:23 +0900

    feat: 자동차 위치 클래스에 `equals()`, `hashCode()` 오버라이딩

--- src/main/kotlin/racingcar/domain/Position.kt
+++ src/main/kotlin/racingcar/domain/Position.kt
@@ -1,5 +1,21 @@
 package racingcar.domain
 
-class Position(value: Int) {
+class Position(
+    private var value: Int
+) {
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Position
+
+        if (value != other.value) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return value
+    }
 }
