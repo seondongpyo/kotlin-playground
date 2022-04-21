64e3b80cd62088adb0c922b4c13ebe18d1b44cd5
seondongpyo <seondongpyo@gmail.com>
2022-04-21 21:40:32 +0900

    feat: 사용자 입력 시 안내 메시지 추가, 애플리케이션 실행용 메인 메서드 추가

---
+++ src/main/kotlin/racingcar/RacingGameMain.kt
@@ -0,0 +1,10 @@
+package racingcar
+
+import racingcar.view.InputView
+
+fun main() {
+    val carNames = InputView.carNames()
+    val attemptCount = InputView.attemptCount()
+
+
+}
--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -8,10 +8,12 @@ class InputView {
         private val scanner = Scanner(System.`in`)
 
         fun carNames(): String {
+            println("자동차 이름 입력(5자 이하, ','로 구분): ")
             return scanner.next()
         }
 
         fun attemptCount(): Int {
+            println("시도 횟수 입력: ")
             return scanner.nextInt()
         }
     }
