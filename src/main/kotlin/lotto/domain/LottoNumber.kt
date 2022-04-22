0c0cae4ec83a230d87151c4712ecaab7c341aec7
seondongpyo <seondongpyo@gmail.com>
2022-04-22 20:26:59 +0900

    feat: [통과] 로또 번호 생성 시 인자로 받은 숫자를 검증

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -4,6 +4,12 @@ class LottoNumber(
     private val value: Int
 ) {
 
+    init {
+        if (value < 1 || value > 45) {
+            throw IllegalArgumentException()
+        }
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
