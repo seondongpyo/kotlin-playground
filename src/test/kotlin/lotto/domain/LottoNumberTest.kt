19b55fcffced44c12d72c1c3942a1b9b6cf7fd9f
seondongpyo <seondongpyo@gmail.com>
2022-04-22 20:25:56 +0900

    test: [실패] 유효하지 않은 숫자로 로또 번호 생성 시도

--- src/test/kotlin/lotto/domain/LottoNumberTest.kt
+++ src/test/kotlin/lotto/domain/LottoNumberTest.kt
@@ -1,16 +1,25 @@
 package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class LottoNumberTest {
 
-    @DisplayName("유효한 로또 숫자")
+    @DisplayName("유효한 로또 번호")
     @ValueSource(ints = [1, 45])
     @ParameterizedTest
     fun valid(value: Int) {
         assertThat(LottoNumber(value)).isEqualTo(LottoNumber(value))
+    }
+
+    @DisplayName("유효하지 않은 로또 번호")
+    @Test
+    fun invalid() {
+        assertThatThrownBy { LottoNumber(0) }
+            .isInstanceOf(IllegalArgumentException::class.java)
     }
 }
