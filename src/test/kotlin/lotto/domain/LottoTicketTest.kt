3f13cec68908d7b29a313c42c04134d11e37e8db
seondongpyo <seondongpyo@gmail.com>
2022-04-23 21:44:24 +0900

    test: [실패] 인자로 전달된 번호가 중복이라면 로또 티켓 생성 시 예외 발생

--- src/test/kotlin/lotto/domain/LottoTicketTest.kt
+++ src/test/kotlin/lotto/domain/LottoTicketTest.kt
@@ -1,6 +1,7 @@
 package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
@@ -10,5 +11,12 @@ class LottoTicketTest {
     @Test
     fun create() {
         assertThat(LottoTicket(listOf(1, 2, 3, 4, 5, 6))).isEqualTo(LottoTicket(listOf(1, 2, 3, 4, 5, 6)))
+    }
+
+    @DisplayName("인자로 전달된 번호에 중복이 있다면 로또 티켓 생성 시 예외가 발생")
+    @Test
+    fun invalid() {
+        assertThatThrownBy { LottoTicket(listOf(1, 2, 3, 4, 5, 5)) }
+            .isInstanceOf(IllegalArgumentException::class.java)
     }
 }
