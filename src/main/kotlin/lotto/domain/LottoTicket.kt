7c45f8338a2504d9bb107f6f3ecf07ba0431cf88
seondongpyo <seondongpyo@gmail.com>
2022-04-23 21:41:21 +0900

    feat: [통과] LottoTicket 클래스에서 equals(), hashCode() 오버라이딩

--- src/main/kotlin/lotto/domain/LottoTicket.kt
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -2,4 +2,13 @@ package lotto.domain
 
 class LottoTicket(numbers: List<Int>) {
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
 }
