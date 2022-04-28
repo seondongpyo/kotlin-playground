695e2ceb68f2dab3f7327da8524c35cd8b72e043
seondongpyo <seondongpyo@gmail.com>
2022-04-28 23:41:03 +0900

    feat: [성공] `equals()`, `hashCode()` 오버라이딩

--- src/main/kotlin/lotto/domain/WinningLottoTicket.kt
+++ src/main/kotlin/lotto/domain/WinningLottoTicket.kt
@@ -2,4 +2,13 @@ package lotto.domain
 
 class WinningLottoTicket(numbers: List<Int>, bonusNumber: Int) {
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
 }
