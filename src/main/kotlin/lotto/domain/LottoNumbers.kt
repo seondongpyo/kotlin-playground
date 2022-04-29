f2bc9d29c01a0bbe7a30e728164087c541e35891
seondongpyo <seondongpyo@gmail.com>
2022-04-29 23:02:07 +0900

    feat: [통과] 사용자가 입력한 값으로 로또 번호 묶음을 생성

--- src/main/kotlin/lotto/domain/LottoNumbers.kt
+++ src/main/kotlin/lotto/domain/LottoNumbers.kt
@@ -1,16 +1,37 @@
 package lotto.domain
 
+import java.util.stream.Collectors
+
 class LottoNumbers(
-    private val numbers: List<Int>
+    private val numbers: Set<Int>
 ) {
 
     companion object {
         fun from(numbers: String): LottoNumbers {
-            return LottoNumbers(listOf(1, 2, 3, 4, 5, 6))
+            val lottoNumbers = numbers.split(",")
+                .stream()
+                .map { number -> Integer.parseInt(number) }
+                .collect(Collectors.toSet())
+            return LottoNumbers(lottoNumbers)
         }
 
         fun of(vararg number: Int): LottoNumbers {
-            return LottoNumbers(number.toList())
+            return LottoNumbers(number.toSet())
         }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as LottoNumbers
+
+        if (numbers != other.numbers) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return numbers.hashCode()
     }
 }
