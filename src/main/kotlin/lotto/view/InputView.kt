86fe294dac68ff57e73370df1d3e0488e034dc80
seondongpyo <seondongpyo@gmail.com>
2022-04-29 22:23:29 +0900

    feat: 사용자 입력을 받는 View 추가

---
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -0,0 +1,25 @@
+package lotto.view
+
+import java.util.Scanner
+
+class InputView {
+
+    companion object {
+        private val scanner = Scanner(System.`in`)
+
+        fun money(): Int {
+            println("구입금액을 입력해 주세요.")
+            return scanner.nextInt()
+        }
+
+        fun winningNumbers(): String {
+            println("지난 주 당첨 번호를 입력해 주세요.")
+            return scanner.next()
+        }
+
+        fun bonusNumber(): Int {
+            println("보너스 볼을 입력해 주세요.")
+            return scanner.nextInt()
+        }
+    }
+}
