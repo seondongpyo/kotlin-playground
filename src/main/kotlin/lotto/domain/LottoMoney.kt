6d768a33b34da56ae314b29da9222009e6bb0704
seondongpyo <seondongpyo@gmail.com>
2022-05-02 23:34:18 +0900

    feat: [통과] 금액의 액수가 1000원 미만일 경우 예외 발생

--- src/main/kotlin/lotto/domain/LottoMoney.kt
+++ src/main/kotlin/lotto/domain/LottoMoney.kt
@@ -1,6 +1,14 @@
 package lotto.domain
 
-class LottoMoney(value: Int) {
+class LottoMoney(
+    private val value: Int
+) {
+
+    init {
+        if (value < 1_000) {
+            throw IllegalArgumentException()
+        }
+    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
