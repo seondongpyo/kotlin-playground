47aa2fb52c77e63a50c1874e0474a1717cc96c25
seondongpyo <seondongpyo@gmail.com>
2022-05-08 21:33:42 +0900

    test: 모든 당첨 결과 중에서 특정 로또 등수가 몇 번인지 확인

--- src/test/kotlin/lotto/domain/LottoRanksTest.kt
+++ src/test/kotlin/lotto/domain/LottoRanksTest.kt
@@ -14,4 +14,11 @@ class LottoRanksTest {
         val ranks = LottoRanks(listOf(FIRST, SECOND, THIRD, FOURTH, FIFTH, NONE))
         assertThat(ranks.totalPrize()).isEqualTo(BigDecimal.valueOf(2_031_555_000))
     }
+
+    @DisplayName("모든 당첨 결과 중에서 특정 로또 당첨 등수가 몇 번인지 확인")
+    @Test
+    fun countOf() {
+        val ranks = LottoRanks(listOf(FIRST, SECOND, NONE, NONE, NONE))
+        assertThat(ranks.countOf(NONE)).isEqualTo(3)
+    }
 }
