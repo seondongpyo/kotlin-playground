5976098e4f4a426ff0826755f8ae218a32c4c33b
seondongpyo <seondongpyo@gmail.com>
2022-05-30 23:04:09 +0900

    refactor: for loop 대신 repeat 활용

--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -11,17 +11,9 @@ class ResultView {
         fun showPosition(cars: List<Car>) {
             val builder = StringBuilder()
             for (car in cars) {
-                builder.append("%s : %s%n".format(car.name(), positionToSymbol(car.position())))
+                builder.append("%s : %s%n".format(car.name(), SYMBOL.repeat(car.position())))
             }
             println(builder.toString())
-        }
-
-        private fun positionToSymbol(position: Int): String {
-            val builder = StringBuilder()
-            for (index in 0 until position) {
-                builder.append(SYMBOL)
-            }
-            return builder.toString()
         }
 
         fun showWinners(winners: List<Car>) {
